c86d3cc152b57124a3ddd85be4536c9fc78a2218
Mark Montes <mam25@pdx.edu>
2020-10-26 15:59:24 -0700

    Functions but slow because too many for loops..

--- conway.cpp
+++ conway.cpp
@@ -5,14 +5,27 @@
 #define ALIVE "X"
 using namespace std;
 
-const int start_x = COLS/2;
-const int start_y = LINES/2;
+struct cell
+{
+    int state;
+    int nCount;
+};
+//PROTOTYPES
+void fill_arr(vector<vector<struct cell>> &);
+int pausescrn(vector<vector<struct cell>> &, int &, int &, int &);
+int printscrn(vector<vector<struct cell>> &);
+int countadj(vector<vector<struct cell>>, int, int);
+void updatemtrx(vector<vector<struct cell>> &);
+int playscrn(vector<vector<struct cell>> &);
+int set_nCount(vector<vector<struct cell>> &);
 
-void fill_arr(vector<vector<int>> &);
-int pausescrn(vector<vector<int>> &, int &, int &, int &);
-int printscrn(vector<vector<int>> &);
-int countadj(vector<vector<int>>, int, int);
-void updatemtrx(vector<vector<int>> &);
+
+
+
+
+
+
+
 
 int main(void)
 {
@@ -23,36 +36,55 @@ int main(void)
     keypad(stdscr, TRUE);
     int curr_x = 0;
     int curr_y = 0;
-    vector<vector<int>> array;
+    vector<vector<struct cell>> array;
     int key;
-
     fill_arr(array); 
     key = getch();
-    pausescrn(array, key, curr_x, curr_y);
+
     do{
-    updatemtrx(array);
-    printscrn(array);
-    wrefresh(stdscr);
-    }while(getch() != (char)'q');
+        key = pausescrn(array, key, curr_x, curr_y);
+        if(key == (int)'p')
+            key = playscrn(array);
+    }while(key != (int)'q');
 
     endwin();
 
     return 0;
 }
 
-void fill_arr(vector<vector<int>> &array)
+
+
+
+
+
+
+
+
+
+//FUNCTIONS
+void fill_arr(vector<vector<struct cell>> &array)
 {
+    vector <struct cell> temp;
     for(int i = 0; i < LINES; i++)
     {
-        vector<int>temp;
         for(int j = 0; j < COLS; j++)
-            temp.push_back(0);
-        
+        {
+            temp.push_back(cell());
+        }
+
         array.push_back(temp);
+    }
+    for(int i = 0; i < LINES; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            array[i][j].state = 0;
+            array[i][j].nCount = 0;
+        }
     }
 }
 
-int pausescrn(vector<vector<int>> &array, int &key, int &curr_x, int &curr_y)
+int pausescrn(vector<vector<struct cell>> &array, int &key, int &curr_x, int &curr_y)
 {
     do
     {
@@ -79,28 +111,32 @@ int pausescrn(vector<vector<int>> &array, int &key, int &curr_x, int &curr_y)
             break;
 
             case (int)'x':
-            if(array[curr_y][curr_x] == 0)
-                array[curr_y][curr_x] = 1;
-            else if(array[curr_y][curr_x] == 1)
-                array[curr_y][curr_x] = 0;
-            key = 0;
+            if(array[curr_y][curr_x].state == 0)
+                array[curr_y][curr_x].state = 1;
+            else if(array[curr_y][curr_x].state == 1)
+                array[curr_y][curr_x].state = 0;
+            break;
+
+            case (int)'n':
+            updatemtrx(array);
+            return 0;
             break;
         }
-
+    wrefresh(stdscr);
     printscrn(array);
     key = mvgetch(curr_y, curr_x);
-    }while(key != (int)'p');
+    }while(key != (int)'p' && key != (int)'q');
 
     return key;
 }
 
-int printscrn(vector<vector<int>> &array)
+int printscrn(vector<vector<struct cell>> &array)
 {
     for(int i = 0; i < LINES; i++)
     {
         for(int j = 0; j < COLS; j++)
         {
-            if(array[i][j] == 1)
+            if(array[i][j].state == 1)
                 mvprintw(i , j, ALIVE);
             else
                 mvprintw(i ,j , DEAD);
@@ -111,47 +147,74 @@ int printscrn(vector<vector<int>> &array)
     return 0;
 }
 
-int countadj(vector<vector<int>> array, int y, int x)
+int countadj(vector<vector<struct cell>> array, int y, int x)
 {
     int sum = 0;
-    if(array[y-1][x] == 1)
-        sum++;
-    if(array[y-1][x-1] == 1)
-        sum++;
-    if(array[y-1][x+1] == 1)
-        sum++;
-    if(array[y][x+1] == 1)
-        sum++; 
-    if(array[y][x-1] == 1)
-        sum++;
-    if(array[y+1][x+1] == 1)
-        sum++;
-    if(array[y+1][x] == 1)
-        sum++;
-    if(array[y+1][x-1] == 1)
-        sum++;
-
+     if(y == 0 || y == LINES-1 || x == 0 || x == COLS-1)
+                return 0;
+    if(array[y-1][x].state == 1)
+        ++sum;
+    if(array[y-1][x-1].state == 1)
+        ++sum;
+    if(array[y-1][x+1].state == 1)
+        ++sum;
+    if(array[y][x+1].state == 1)
+        ++sum; 
+    if(array[y][x-1].state == 1)
+        ++sum;
+    if(array[y+1][x+1].state == 1)
+        ++sum;
+    if(array[y+1][x].state == 1)
+        ++sum;
+    if(array[y+1][x-1].state == 1)
+        ++sum;
     return sum;
 }
 
-void updatemtrx(vector<vector<int>> &array)
+void updatemtrx(vector<vector<struct cell>> &array)
 {
-    for(int i = 0; i < LINES-1; i++)
+    set_nCount(array);
+    for(int i = 0; i < LINES; i++)
     {
-        for(int j = 0; j < COLS-1; j++)
+        for(int j = 0; j < COLS; j++)
         {
-            int n;
-            if(i == 0 || i == COLS-1 || j == 0 || j == LINES-1)
-                n = 0;
+            if(i == 0 || i == LINES-1 || j == 0 || j == COLS-1)
+                array[i][j].state = 0;
             else
-                n = countadj(array, i, j);
-            if(n < 2)
-                array[i][j] = 0;
-            if(n > 3)
-                array[i][j] = 0;
-            if(n == 3)
-                array[i][j] = 1;
+            {
+            if(array[i][j].state == 0 && array[i][j].nCount == 3)
+                array[i][j].state = 1;
+            else if(array[i][j].state == 1 && (array[i][j].nCount < 2 || array[i][j].nCount > 3))
+                array[i][j].state = 0;
+            }
         }
 
     }
+}
+
+int playscrn(vector<vector<struct cell>> &array)
+{
+    int key;
+    do{
+       updatemtrx(array);
+       printscrn(array);
+       wrefresh(stdscr);
+       key = getch();
+    }while(key != (int)'p' && key != (int)'q');
+    return key;
+}
+
+int set_nCount(vector<vector<struct cell>> &array)
+{
+    for(int i = 0; i < LINES; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            if(i == 0 || i == LINES-1 || j == 0 || j == COLS-1)
+                array[i][j].nCount = 0;
+            else
+                array[i][j].nCount = countadj(array, i, j);
+        }
+    }
+    return 0;
 }
