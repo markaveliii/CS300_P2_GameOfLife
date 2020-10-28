fb402117193eeda8285365b939e77a16601989a1
Mark Montes <mam25@pdx.edu>
2020-10-28 16:14:10 -0700

    Finished product just needs a 0.5 second delay between frames

--- conway.cpp
+++ conway.cpp
@@ -3,6 +3,7 @@
 #include <curses.h>
 #define DEAD " "
 #define ALIVE "X"
+#define QUIT (int)'q'
 using namespace std;
 
 const int start_x = COLS/2;
@@ -13,6 +14,7 @@ int pausescrn(vector<vector<int>> &, int &, int &, int &);
 int printscrn(vector<vector<int>> &);
 int countadj(vector<vector<int>>, int, int);
 void updatemtrx(vector<vector<int>> &);
+int playscrn(vector<vector<int>> &);
 
 int main(void)
 {
@@ -28,12 +30,12 @@ int main(void)
 
     fill_arr(array); 
     key = getch();
-    pausescrn(array, key, curr_x, curr_y);
+
     do{
-    updatemtrx(array);
-    printscrn(array);
-    wrefresh(stdscr);
-    }while(getch() != (char)'q');
+        key = pausescrn(array, key, curr_x, curr_y);
+        if(key == (int)'p')
+            key = playscrn(array);
+    }while(key != QUIT);
 
     endwin();
 
@@ -88,13 +90,12 @@ int pausescrn(vector<vector<int>> &array, int &key, int &curr_x, int &curr_y)
 
             case (int)'n':
             updatemtrx(array);
-            printscrn(array);
             break;
         }
 
     printscrn(array);
     key = mvgetch(curr_y, curr_x);
-    }while(key != (int)'p');
+    }while(key != (int)'p' && key != QUIT);
 
     return key;
 }
@@ -119,7 +120,7 @@ int printscrn(vector<vector<int>> &array)
 int countadj(vector<vector<int>> array, int y, int x)
 {
     int sum = 0;
-    if(array[y-1][x] == 1)
+/*    if(array[y-1][x] == 1)
         sum++;
     if(array[y-1][x-1] == 1)
         sum++;
@@ -135,36 +136,53 @@ int countadj(vector<vector<int>> array, int y, int x)
         sum++;
     if(array[y+1][x-1] == 1)
         sum++;
-
+*/
+    for(int i = -1; i < 2; i++)
+    {
+        for(int j = -1; j < 2; j++)
+            sum += array[i+y][j+x];
+    }
+    sum -= array[y][x];
     return sum;
 }
 
 void updatemtrx(vector<vector<int>> &array)
 {
     vector<vector<int>> newgen;
-    for(int i = 0; i < LINES-1; i++)
+    for(int i = 0; i < LINES; i++)
     {
         vector<int>temp;
-        for(int j = 0; j < COLS-1; j++)
+        for(int j = 0; j < COLS; j++)
         {
             int n;
-            if(i == 0 || i == COLS-1 || j == 0 || j == LINES-1)
+            if(i == 0 || i == LINES-1 || j == 0 || j == COLS-1)
                 temp.push_back(0);
             else
             {
-                 n = countadj(array, i, j);
-                if(n < 2)
-                    temp.push_back(0);
-                if(n > 3)
-                    temp.push_back(0);
-                if(n == 3)
+                n = countadj(array, i, j);
+                if(array[i][j] == 0 && n == 3)
                     temp.push_back(1);
+                else if(array[i][j] == 1 && (n > 3 ||n < 2) )
+                    temp.push_back(0);
+                else
+                    temp.push_back(array[i][j]);
             }
         }
         newgen.push_back(temp);
 
     }
     array = newgen;
+}
+
+int playscrn(vector<vector<int>> &array)
+{
+    int key;
+    do{
+       updatemtrx(array);
+       printscrn(array);
+       key = getch();
+    }while(key != (int)'p' && key != (int)'q');
+    return key;
 }
 
 
